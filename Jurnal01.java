54c063faedc399ebc5914f704c1a5cc861fedd74
Ghinayolanda <145578514+Ghinayolanda@users.noreply.github.com>
2023-09-28 17:20:12 +0700

    Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main (String[] args) {
+        System.out.println("NAMA: Ghina Aniqah Yolanda Chaniago");
+        System.out.println("NIM: 607062330096");
+        System.out.println("Kelas: D3IF-47-01");
+    }
+}
